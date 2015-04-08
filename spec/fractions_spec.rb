b80ced0f980683be3978b097c014974e70b8b822
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-04-08 14:07:39 +0100

    Listing the specs to be written

---
+++ spec/fractions_spec.rb
@@ -0,0 +1,11 @@
+describe "Adding fractions" do
+ it "adds two +ve numbers to give a +ve sum"
+
+ it "adds a +ve number to a small -ve to give a +ve sum"
+
+ it "adds a +ve number to a -ve number of equal magnitude to give 0"
+
+ it "adds a small +ve number to a large -ve number to give a -ve sum"
+
+ it "adds two -ve numbers to yield another negative number"
+end
