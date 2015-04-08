39043b7bf3ccdcc3007df81e0b9da6008bd04a72
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-04-08 16:24:26 +0100

    We can a +ve number to a -ve number of equal magnitude to give zero

--- spec/fractions_spec.rb
+++ spec/fractions_spec.rb
@@ -13,7 +13,9 @@ describe "Adding fractions" do
     expect(Fraction.new(2).plus(Fraction.new(-1)).int_value).to eq(1)
  end 
 
- it "adds a +ve number to a -ve number of equal magnitude to give 0"
+ it "adds a +ve number to a -ve number of equal magnitude to give 0" do
+    expect(Fraction.new(1).plus(Fraction.new(-1)).int_value).to eq(0)
+ end
 
  it "adds a small +ve number to a large -ve number to give a -ve sum"
 
