6e7dbee245fa0963792287780a69ee22778a86cf
Hemal <h.n.varambhia@gmail.com>
2015-04-10 15:15:55 +0100

    Put class into a math module

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -1,21 +1,24 @@
-class Fraction
- attr_reader :numerator, :denominator
- 
- def initialize(numerator, denominator = 1)
-   gcd = numerator.gcd(denominator)
-   @numerator = numerator/gcd
-   @denominator = denominator/gcd
- end
+module Math
+  class Fraction
+    attr_reader :numerator, :denominator
 
- def plus(another)
-   sum_denominator = @denominator * another.denominator
-   Fraction.new(
-	@numerator*(sum_denominator/@denominator) + 
-	another.numerator*(sum_denominator/another.denominator), 
-	sum_denominator)
- end
+    def initialize(numerator, denominator = 1)
+     gcd = numerator.gcd(denominator)
+     @numerator = numerator/gcd
+     @denominator = denominator/gcd
+    end
 
- def int_value
-   @numerator
- end
+    def plus(another)
+     sum_denominator = @denominator * another.denominator
+     Fraction.new(
+        @numerator*(sum_denominator/@denominator) +
+        another.numerator*(sum_denominator/another.denominator),
+        sum_denominator)
+    end
+
+    def int_value
+     @numerator
+    end
+  end
 end
+
