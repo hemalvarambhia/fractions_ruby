a9c0e937a59a5df58e80df9a1d79dfe322eebc1b
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-04-11 17:32:16 +0100

    Multiplication by 0 and fractions where denominator and numerator cancel checked

--- spec/fractions_spec.rb
+++ spec/fractions_spec.rb
@@ -105,6 +105,12 @@ describe "Equating fractions" do
 end
 
 describe "Multiplying fractions" do
+   context "multiplying a fraction by 0" do
+    it "computes 0" do
+      expect(Math::Fraction.new(0).times(Math::Fraction.new(1, 3))).to eq(Math::Fraction.new(0))
+    end
+   end
+  
   context "fraction multiplied by unity" do
     it "yields the original fraction" do
       expect(Math::Fraction.new(1, 2).times(Math::Fraction.new(1))).to eq(Math::Fraction.new(1, 2))
@@ -117,15 +123,21 @@ describe "Multiplying fractions" do
     end
   end
 
-  context "fractions multiplied by fraction" do
+  context "fraction multiplied by fraction" do
     it "computes the product correctly" do
       expect(Math::Fraction.new(1, 2).times(Math::Fraction.new(1, 2))).to eq(Math::Fraction.new(1, 4))
       expect(Math::Fraction.new(1, 3).times(Math::Fraction.new(1, 4))).to eq(Math::Fraction.new(1, 12))
     end
   end 
 
-  context "fraction multiplied by a integer that cancels denominator" do
-    it "computed the product correctly" do
+  context "fraction multiplied by another fraction where denominator and numerator cancel" do
+    it "computes the product correctly" do
+      expect(Math::Fraction.new(1, 2).times(Math::Fraction.new(2, 3))).to eq(Math::Fraction.new(1, 3))
+    end
+  end
+
+  context "fraction multiplied by an integer that cancels denominator" do
+    it "computes the product correctly" do
       expect(Math::Fraction.new(1, 2).times(Math::Fraction.new(2))).to eq(Math::Fraction.new(1))
       expect(Math::Fraction.new(4).times(Math::Fraction.new(1, 2))).to eq(Math::Fraction.new(2))
     end
