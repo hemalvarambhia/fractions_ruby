0428d98cc083b367c5c5308689742569d4558790
Hemal <h.n.varambhia@gmail.com>
2015-04-14 13:23:58 +0100

    Specs confirm that eqls is relfexive, symmetric and transitive

--- spec/fractions_spec.rb
+++ spec/fractions_spec.rb
@@ -79,6 +79,29 @@ describe "Reducing fractions" do
 end
 
 describe "Equating fractions" do
+ it "is reflexive" do
+    fraction = Math::Fraction.new(1, 4)
+    expect(fraction).to eq(fraction)
+ end
+  
+ it "is transitive" do
+    fraction_1 = Math::Fraction.new(1, 4)
+    fraction_2 = Math::Fraction.new(1, 4)
+    fraction_3 = Math::Fraction.new(1, 4)
+
+    expect(fraction_1).to eq(fraction_2)
+    expect(fraction_2).to eq(fraction_3)
+    expect(fraction_1).to eq(fraction_3)
+ end 
+
+ it "is symmetric" do
+    fraction_1 = Math::Fraction.new(1, 4)
+    fraction_2 = Math::Fraction.new(1, 4)
+    
+    expect(fraction_1).to eq(fraction_2)
+    expect(fraction_2).to eq(fraction_1)
+ end
+
  it "marks two fractions with the same numerator and denominator" do
     expect(Math::Fraction.new(1, 2)).to eq(Math::Fraction.new(1, 2))
  end
